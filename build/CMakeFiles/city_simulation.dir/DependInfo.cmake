47aa22be14b709d9594bd2d4b841f273b67279c2
shadinso <shadinsorkhi246@gmail.com>
2026-05-01 15:06:27 +0300

    fix city.txt name and add X11 for raylib

---
+++ build/CMakeFiles/city_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/student/city_simulation/dijkstra.c" "CMakeFiles/city_simulation.dir/dijkstra.c.o" "gcc" "CMakeFiles/city_simulation.dir/dijkstra.c.o.d"
+  "/home/student/city_simulation/graph.c" "CMakeFiles/city_simulation.dir/graph.c.o" "gcc" "CMakeFiles/city_simulation.dir/graph.c.o.d"
+  "/home/student/city_simulation/main.c" "CMakeFiles/city_simulation.dir/main.c.o" "gcc" "CMakeFiles/city_simulation.dir/main.c.o.d"
+  "/home/student/city_simulation/renderer.c" "CMakeFiles/city_simulation.dir/renderer.c.o" "gcc" "CMakeFiles/city_simulation.dir/renderer.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/city_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/city_simulation.dir/dijkstra.c.o"
+  "CMakeFiles/city_simulation.dir/dijkstra.c.o.d"
+  "CMakeFiles/city_simulation.dir/graph.c.o"
+  "CMakeFiles/city_simulation.dir/graph.c.o.d"
+  "CMakeFiles/city_simulation.dir/main.c.o"
+  "CMakeFiles/city_simulation.dir/main.c.o.d"
+  "CMakeFiles/city_simulation.dir/renderer.c.o"
+  "CMakeFiles/city_simulation.dir/renderer.c.o.d"
+  "city_simulation"
+  "city_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/city_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
