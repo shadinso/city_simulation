0b0268380c6dfb7950a2c37838279d6c9401b473
alaa-eweisat <alaaewisath@gmail.com>
2026-06-12 17:53:55 +0300

    Add milestone 6 synchronization

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
+/*
+ * main.c - Milestone 6: Node Synchronization using POSIX Semaphores
+ *
+ * At any given moment, only ONE traveler may occupy a node.
+ * Others wait outside (blocked on the semaphore).
+ * The GUI shows waiting travelers in a different color (YELLOW).
+ *
+ * IPC: pipes (same as milestone 5)
+ * Sync: POSIX named semaphores, one per node (/city_node_0, /city_node_1, ...)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <semaphore.h>
 
 #include "raylib.h"
 #include "graph.h"
@@ -12,9 +25,12 @@
 #include "renderer.h"
 
 #define MAX_TRAVELERS 20
+#define SEM_NAME_PREFIX "/city_node_"
 
-#define MSG_NODE 1
-#define MSG_FINISHED 2
+#define MSG_NODE      1
+#define MSG_FINISHED  2
+#define MSG_WAITING   3
+#define MSG_ENTERED   4
 
 typedef struct {
     int type;
@@ -33,6 +49,7 @@ typedef struct {
     Color color;
     int active;
     int finished;
+    int waiting;
 
     int current_node;
     int next_node;
@@ -40,6 +57,10 @@ typedef struct {
     float x;
     float y;
 } Traveler;
+
+static void make_sem_name(char *buf, int node_id) {
+    sprintf(buf, "%s%d", SEM_NAME_PREFIX, node_id);
+}
 
 static int get_edge_weight(Graph *g, int from, int to) {
     Edge *e = g->nodes[from].head;
@@ -50,8 +71,8 @@ static int get_edge_weight(Graph *g, int from, int to) {
     return 1;
 }
 
-static int read_milestone5_file(const char *filename, Graph **out_graph,
-                                Traveler travelers[], int *traveler_count) {
+static int read_input_file(const char *filename, Graph **out_graph,
+                           Traveler travelers[], int *traveler_count) {
     FILE *fp = fopen(filename, "r");
     if (!fp) {
         fprintf(stderr, "Error: cannot open file '%s'\n", filename);
@@ -128,6 +149,7 @@ static int read_milestone5_file(const char *filename, Graph **out_graph,
         travelers[i].pipe_fd[1] = -1;
         travelers[i].active = 1;
         travelers[i].finished = 0;
+        travelers[i].waiting = 0;
         travelers[i].current_node = src;
         travelers[i].next_node = -1;
         travelers[i].x = 0;
@@ -135,20 +157,19 @@ static int read_milestone5_file(const char *filename, Graph **out_graph,
     }
 
     fclose(fp);
-
     *out_graph = g;
     *traveler_count = count;
     return 0;
 }
 
-static void send_message(int fd, int type, int index, int current_node, int next_node) {
+static void send_message(int fd, int type, int index,
+                         int current_node, int next_node) {
     IPCMessage msg;
     msg.type = type;
     msg.pid = getpid();
     msg.traveler_index = index;
     msg.current_node = current_node;
     msg.next_node = next_node;
-
     write(fd, &msg, sizeof(msg));
 }
 
@@ -167,25 +188,41 @@ static void child_process(Graph *g, Traveler t, int index, int write_fd) {
 
     for (int i = 0; i < path_len; i++) {
         int current = path[i];
-        int next = -1;
+        int next = (i < path_len - 1) ? path[i + 1] : -1;
 
-        if (i < path_len - 1) {
-            next = path[i + 1];
+        char sem_name[64];
+        make_sem_name(sem_name, current);
+
+        sem_t *node_sem = sem_open(sem_name, 0);
+        if (node_sem == SEM_FAILED) {
+            perror("sem_open");
+            node_sem = NULL;
+        } else {
+            send_message(write_fd, MSG_WAITING, index, current, next);
+
+            if (sem_wait(node_sem) == -1) {
+                perror("sem_wait");
+            }
         }
 
+        send_message(write_fd, MSG_ENTERED, index, current, next);
         send_message(write_fd, MSG_NODE, index, current, next);
-
-        if (i == path_len - 1) {
-            break;
-        }
-
-        int weight = get_edge_weight(g, current, next);
-        if (weight <= 0) weight = 1;
-
-        usleep(weight * 300000);
 
         if (i > 0 && i < path_len - 1) {
             sleep(1);
+        }
+
+        if (node_sem) {
+            if (sem_post(node_sem) == -1) {
+                perror("sem_post");
+            }
+            sem_close(node_sem);
+        }
+
+        if (next != -1) {
+            int weight = get_edge_weight(g, current, next);
+            if (weight <= 0) weight = 1;
+            usleep(weight * 300000);
         }
     }
 
@@ -199,30 +236,42 @@ static void child_process(Graph *g, Traveler t, int index, int write_fd) {
 static void handle_message(IPCMessage msg, Traveler travelers[], NodeInfo *info) {
     int i = msg.traveler_index;
 
-    if (msg.type == MSG_NODE) {
-        travelers[i].current_node = msg.current_node;
-        travelers[i].next_node = msg.next_node;
+    switch (msg.type) {
+        case MSG_WAITING:
+            travelers[i].waiting = 1;
+            printf("[PID=%d] waiting outside node %d\n", msg.pid, msg.current_node);
+            fflush(stdout);
+            break;
 
-        travelers[i].x = info[msg.current_node].x;
-        travelers[i].y = info[msg.current_node].y;
+        case MSG_ENTERED:
+            travelers[i].waiting = 0;
+            break;
 
-        if (msg.next_node == -1) {
-            printf("[PID=%d] arrived at node %d | DESTINATION\n",
-                   msg.pid, msg.current_node);
-        } else {
-            printf("[PID=%d] arrived at node %d | next node: %d\n",
-                   msg.pid, msg.current_node, msg.next_node);
-        }
+        case MSG_NODE:
+            travelers[i].current_node = msg.current_node;
+            travelers[i].next_node = msg.next_node;
+            travelers[i].x = info[msg.current_node].x;
+            travelers[i].y = info[msg.current_node].y;
 
-        fflush(stdout);
-    }
+            if (msg.next_node == -1) {
+                printf("[PID=%d] arrived at node %d | DESTINATION\n",
+                       msg.pid, msg.current_node);
+            } else {
+                printf("[PID=%d] arrived at node %d | next node: %d\n",
+                       msg.pid, msg.current_node, msg.next_node);
+            }
 
-    if (msg.type == MSG_FINISHED) {
-        travelers[i].finished = 1;
-        travelers[i].active = 0;
+            fflush(stdout);
+            break;
 
-        printf("[PID=%d] finished\n", msg.pid);
-        fflush(stdout);
+        case MSG_FINISHED:
+            travelers[i].finished = 1;
+            travelers[i].active = 0;
+            travelers[i].waiting = 0;
+
+            printf("[PID=%d] finished\n", msg.pid);
+            fflush(stdout);
+            break;
     }
 }
 
@@ -236,7 +285,7 @@ int main(int argc, char *argv[]) {
     Traveler travelers[MAX_TRAVELERS];
     int traveler_count = 0;
 
-    if (read_milestone5_file(argv[1], &g, travelers, &traveler_count) != 0) {
+    if (read_input_file(argv[1], &g, travelers, &traveler_count) != 0) {
         return 1;
     }
 
@@ -247,6 +296,20 @@ int main(int argc, char *argv[]) {
     }
 
     build_city_layout(g, info);
+
+    sem_t *node_sems[MAX_NODES];
+
+    for (int i = 0; i < g->num_nodes; i++) {
+        char name[64];
+        make_sem_name(name, i);
+        sem_unlink(name);
+
+        node_sems[i] = sem_open(name, O_CREAT | O_EXCL, 0666, 1);
+        if (node_sems[i] == SEM_FAILED) {
+            perror("sem_open (create)");
+            node_sems[i] = NULL;
+        }
+    }
 
     Color colors[MAX_TRAVELERS] = {
         RED, BLUE, GREEN, ORANGE, PURPLE,
@@ -285,18 +348,22 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "City Simulation - Milestone 5 IPC");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "City Simulation - Milestone 6 Sync");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         for (int i = 0; i < traveler_count; i++) {
-            if (travelers[i].pipe_fd[0] != -1) {
-                IPCMessage msg;
-                ssize_t bytes;
+            if (travelers[i].pipe_fd[0] == -1) {
+                continue;
+            }
 
-                while ((bytes = read(travelers[i].pipe_fd[0], &msg, sizeof(msg))) == sizeof(msg)) {
-                    handle_message(msg, travelers, info);
-                }
+            IPCMessage msg;
+            ssize_t bytes;
+
+            while ((bytes = read(travelers[i].pipe_fd[0],
+                                 &msg,
+                                 sizeof(msg))) == sizeof(msg)) {
+                handle_message(msg, travelers, info);
             }
         }
 
@@ -306,35 +373,60 @@ int main(int argc, char *argv[]) {
         draw_graph(g, info);
 
         for (int i = 0; i < traveler_count; i++) {
-            if (!travelers[i].finished) {
-                DrawCircle((int)travelers[i].x,
-                           (int)travelers[i].y,
-                           10,
-                           travelers[i].color);
-
-                char label[10];
-                sprintf(label, "T%d", i + 1);
-
-                DrawText(label,
-                         (int)travelers[i].x + 12,
-                         (int)travelers[i].y - 8,
-                         14,
-                         WHITE);
+            if (travelers[i].finished) {
+                continue;
             }
+
+            Color display_color = travelers[i].waiting ? YELLOW : travelers[i].color;
+            int radius = travelers[i].waiting ? 7 : 10;
+
+            int wait_rank = 0;
+            for (int j = 0; j < i; j++) {
+                if (travelers[j].waiting) {
+                    wait_rank++;
+                }
+            }
+
+            int offset = travelers[i].waiting ? (18 + wait_rank * 14) : 0;
+
+            DrawCircle((int)travelers[i].x,
+                       (int)travelers[i].y + offset,
+                       radius,
+                       display_color);
+
+            char label[16];
+            sprintf(label, travelers[i].waiting ? "W%d" : "T%d", i + 1);
+
+            DrawText(label,
+                     (int)travelers[i].x + 12,
+                     (int)travelers[i].y - 8 + offset,
+                     14,
+                     WHITE);
         }
 
-        int all_finished = 1;
+        DrawText("T# = traveling   W# = waiting for node",
+                 20,
+                 SCREEN_HEIGHT - 60,
+                 16,
+                 RAYWHITE);
+
+        DrawText("Milestone 6 - Node Sync (semaphores)",
+                 20,
+                 SCREEN_HEIGHT - 35,
+                 20,
+                 RAYWHITE);
+
+        int all_done = 1;
         for (int i = 0; i < traveler_count; i++) {
             if (!travelers[i].finished) {
-                all_finished = 0;
+                all_done = 0;
+                break;
             }
         }
 
-        if (all_finished) {
+        if (all_done) {
             DrawText("ALL FINISHED!", 20, 30, 30, GREEN);
         }
-
-        DrawText("Milestone 5 - IPC using pipes", 20, SCREEN_HEIGHT - 35, 20, RAYWHITE);
 
         EndDrawing();
     }
@@ -349,9 +441,19 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    for (int i = 0; i < g->num_nodes; i++) {
+        char name[64];
+        make_sem_name(name, i);
+
+        if (node_sems[i]) {
+            sem_close(node_sems[i]);
+        }
+
+        sem_unlink(name);
+    }
+
     free(info);
     free_graph(g);
-
     CloseWindow();
 
     return 0;
